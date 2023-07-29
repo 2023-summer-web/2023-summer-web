d10666e5edb636e04d845a7a0b6945964dc1e8d6
Yadorigi <leyancui@foxmail.com>
2023-07-29 18:29:58 +0800

    refa: code review

--- scripts/main.js
+++ scripts/main.js
@@ -3,53 +3,40 @@ export const ctx = canvas.getContext('2d');
 
 export const canvasContainer = document.getElementById('canvas-container');
 
+let scale = 1.0;
+
 export const dataInputTable = document.getElementById('dataInputTable').getElementsByTagName('tbody')[0];
 
-const addRowBtn = document.getElementById('addRowBtn');
-const saveChartBtn = document.getElementById("saveChartBtn");
+export const sidebar = document.getElementById('sidebar');
 
-/// Add a new row to the table
-addRowBtn.addEventListener('click', () => {
-    const lastRow = dataInputTable.lastElementChild;
-    const lastYear = lastRow.querySelector('.year').value;
-    const defaultValue = (lastYear === '') ? '' : Number(lastYear) + 1;
+export const addRowBtn = document.getElementById('addRowBtn');
+export const saveChartBtn = document.getElementById("saveChartBtn");
 
-    // Add a new row
-    const newRow = document.createElement('tr');
-    newRow.className = 'dataRow';
-    newRow.innerHTML = `
-        <td><input type="number" class="year" value="${defaultValue}"></td>
-        <td><input type="number" class="yield"></td>
-        <td><button class="deleteBtn">×</button></td>
-    `;
-    dataInputTable.appendChild(newRow);
-
-    // Focus on the new row
-    newRow.querySelector('.yield').focus();
-
-    // Change the delete button status
-    const rows = document.querySelectorAll('.dataRow');
-    const deleteButtons = document.querySelectorAll('.deleteBtn');
-    deleteButtons.forEach(button => {
-        if (rows.length > 1) {
-            button.disabled = false;
-        } else {
-            button.disabled = true;
-        }
-    });
-});
-
-saveChartBtn.addEventListener("click", function() {
-    const imgURL = canvas.toDataURL("image/png");
-
-    // Create a virtual link element
-    const downloadLink = document.createElement("a");
-    downloadLink.href = imgURL;
-    downloadLink.target = "_blank";
-
-    const filename = prompt("请输入文件名", "chart.png");
-    if (filename !== null && filename.trim() !== "") {
-        downloadLink.download = filename;
-        downloadLink.click();
+/// Mouse wheel event listener
+document.addEventListener('wheel', (event) => {
+    const sidebarRect = sidebar.getBoundingClientRect();
+    const canvasContainerRect = canvasContainer.getBoundingClientRect();
+    if (event.clientX >= sidebarRect.left && event.clientX <= sidebarRect.right &&
+        event.clientY >= sidebarRect.top && event.clientY <= sidebarRect.bottom) {
+        sidebar.scrollTop += event.deltaY;
+    } else if (event.clientX >= canvasContainerRect.left && event.clientX <= canvasContainerRect.right &&
+        event.clientY >= canvasContainerRect.top && event.clientY <= canvasContainerRect.bottom) {
+        // Zoom the canvas
+        const delta = Math.max(-1, Math.min(1, event.deltaY));
+        const zoomStep = 0.005;
+    
+        // Scaling range: [0.75, 1.5]
+        // Canvas size range: [600, 450] ~ [1200, 900] (initial size: 800, 600)
+        scale = Math.max(0.75, Math.min(1.5, scale + delta * zoomStep));
+    
+        // Scale the canvas with the center point as the origin
+        canvas.style.transformOrigin = 'center center';
+        canvas.style.transform = `scale(${scale})`;
+    
+        // Adjust the canvas container size
+        canvasContainer.style.width = `${800 * scale}px`;
+        canvasContainer.style.height = `${600 * scale}px`;
+    } else {
+        window.scrollBy(0, event.deltaY);
     }
 });
